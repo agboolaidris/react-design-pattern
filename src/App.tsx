ebaadaccb59cab2bed553e55de46bb68a37a29f5
Iris <agboolaisholaidreez>
2023-05-20 22:46:19 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { PortalWithState, Portal } from "react-portal";
+type Props = {
+  items: string[];
+};
+
+const List = ({ items }: Props) => {
+  // Yоur cоdе gоеs hеrе
+  const [sortedItems, setSortedItems] = React.useState(items);
+
+  const handleItemClick = React.useCallback((item: string) => {
+    const removeItem = sortedItems.filter((sortedItem) => sortedItem !== item);
+
+    setSortedItems([item, ...removeItem]);
+  }, []);
 
   return (
-    <div className="App bg-slate-600 text-yellow-300">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <ul>
+      {sortedItems.map((item) => (
+        <li key={item} onClick={() => handleItemClick(item)}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <List items={["A", "B", "C"]} />;
 }
 
-export default App
+export default App;
